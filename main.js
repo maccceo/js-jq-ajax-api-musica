7d4b7f036c7722952fd8695332201c94360b1f54
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-30 17:03:03 +0200

    Bonus: switch tra i generi funzionante

--- main.js
+++ main.js
@@ -10,38 +10,67 @@ var content, html;
 
 
 $(document).ready(function() {
-	// popolo la pagina coi CD
-	$.ajax({
-		url: "https://flynn.boolean.careers/exercises/api/array/music",
-		method: "GET",
-		success: function (data) {
-			CDviewer(data.response);
-		},
-		error: function (richiesta,stato,errori) {
-			alert("Qualcosa è andato storto :/ "+errori);
-			console.log("richiesta: " + richiesta);
-			console.log("stato: " + stato);
-			console.log("errori: " + errori);
-		}
-	})
+	// inizializzo la pagina coi CD del genere di defult
+	CDviewer($(".selected").text());
+
+	// funzione di cambio del genere musicale
+	$(".genre-button").click(function (){
+		changeGenre($(this));
+	});
 });
 
 
 
 
-function CDviewer (albums) {
+function CDviewer (genre) {
+	// recupero dall'API i CD
+	$.ajax({
+		url: "https://flynn.boolean.careers/exercises/api/array/music",
+		method: "GET",
+		success: function (data) {
+			var albums = data.response;
+
+			// rimuovo gli album visti in precedenza
+			$(".cd").remove();
+
+			// controllo tutti gli album:
+			for (var i = 0; i < albums.length; i++) {
+				// se sono del genere richiesto...
+				if (albums[i].genre == genre) {
+					// prelevo i dati dell'album
+					context = {
+						img: albums[i].poster,
+						title: albums[i].title,
+						author: albums[i].author,
+						year: albums[i].year
+					};
+					// li "complilo" nel template
+					html = template(context);
+					// visualizzo l'album nella pagina
+					$(".cds-container").append(html);
+				} // ...sennò niente!				
+			}
+		},
+		error: function (richiesta,stato,errori) {
+			alert("Qualcosa è andato storto :/ " + errori);
+			console.log("richiesta: " + richiesta);
+			console.log("stato: " + stato);
+			console.log("errori: " + errori);
+		}
+	})
+
 	// passo tutti gli album restituiti dall'API
-	for (var i = 0; i < albums.length; i++) {
-		// prelevo i dati dell'album
-		context = {
-			img: albums[i].poster,
-			title: albums[i].title,
-			author: albums[i].author,
-			year: albums[i].year
-		};
-		// li "complilo" nel template
-		html = template(context);
-		// visualizzo l'album nella pagina
-		$(".cds-container").append(html);
-	}
+
+}
+
+function changeGenre (buttonPressed) {
+	// feedback visivo che hai cambiato genere
+	$(".genre-button").removeClass("selected");
+	buttonPressed.addClass("selected");
+
+	// salvo string della nuova classe scelta
+	var genre = buttonPressed.text();
+
+	// ri-interrogo l'API stampando solo gli album del genere giusto
+	CDviewer(genre);
 }
