7b0519bcea8220fc5ab9ae294e06e68714cbd9cd
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-30 16:13:02 +0200

    Esercizio base completato

--- main.js
+++ main.js
@@ -4,7 +4,45 @@
 // Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz.
 // In base a cosa scegliamo nella select vedremo i corrispondenti cd.
 
+// Init template dei CD
+var source   = document.getElementById("cd-template").innerHTML;
+var template = Handlebars.compile(source);
+var content, html;
+
 
 $(document).ready(function() {
-	//Code
+	// popolo la pagina coi CD
+	$.ajax({
+		url: "https://flynn.boolean.careers/exercises/api/array/music",
+		method: "GET",
+		success: function (data) {
+			CDviewer(data.response);
+		},
+		error: function (richiesta,stato,errori) {
+			alert("Qualcosa è andato storto :/ "+errori);
+			console.log("richiesta: " + richiesta);
+			console.log("stato: " + stato);
+			console.log("errori: " + errori);
+		}
+	})
 });
+
+
+
+
+function CDviewer (albums) {
+	// passo tutti gli album restituiti dall'API
+	for (var i = 0; i < albums.length; i++) {
+		// prelevo i dati dell'album
+		context = {
+			img: albums[i].poster,
+			title: albums[i].title,
+			author: albums[i].author,
+			year: albums[i].year
+		};
+		// li "complilo" nel template
+		html = template(context);
+		// visualizzo l'album nella pagina
+		$(".cds-container").append(html);
+	}
+}
